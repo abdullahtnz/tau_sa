cf405297c996d851df20fabfb7b53a916eb0776d
abdullahtnz <novruzluabdullah03@gmail.com>
2026-05-25 00:43:20 +0400

    final

--- static/js/config.js
+++ static/js/config.js
@@ -2,7 +2,7 @@
 // Change API_BASE to your backend URL when deploying separately.
 // Leave empty ("") when frontend is served from the same origin as the backend.
 
-var API_BASE = "http://localhost:8080";
+var API_BASE = "https://tau-sa.onrender.com";
 
 // Example for separate deployment:
 // var API_BASE = "https://your-backend-server.com";
